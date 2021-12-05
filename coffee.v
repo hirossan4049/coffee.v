7c2eecfa9766192313c8aba004d911329af55451
hirossan4049 <haruto405329@gmail.com>
2021-12-06 01:09:11 +0900

    update

--- coffee.v
+++ coffee.v
@@ -8,6 +8,15 @@ import log
 import net
 
 
+const (
+    port = 2525
+)
+
+//const (
+//    error_418 = 
+//)
+
+
 fn main() {
     mut log := log.Log{}
     log.set_level(.info)
@@ -20,9 +29,9 @@ fn main() {
     \\___\\___/|_| |_|  \\___|\\___| (_)   \\_/
 
     ")
-    mut l := net.listen_tcp(.ip6, ':2525') or { panic("error") }
+    mut l := net.listen_tcp(.ip6, ':$port') or { panic("error") }
 
-    log.info("start server localhost:2525")
+    log.info("start server localhost:$port")
 
     for {
         mut conn := l.accept() or {
@@ -50,22 +59,25 @@ fn handle_conn(mut conn net.TcpConn) {
     conn.write("".bytes()) or {}
     mut line := reader.read_line() or { return }
     method, target, version := parse_request_line(line)
-    println(method)
-    println(target)
-    println(version)
 
-    //req := http.parse_request(mut reader) or { return }
-    //println(req)
-    //
+    protocol := version.split("/")[0]
+
+    if protocol == "HTTP" {
+        request_http(mut conn)
+    } else if protocol == "HTCPCP" {
+
+    }
+
+}
+
+fn request_http(mut conn net.TcpConn) {
     data := os.read_file("html/418.html") or {
         panic("cannnot read html/418.html")
         return
     }
-
-    conn.write(("
+    conn.write(("\
 HTTP/1.1 418 OK
-Content-Type: text/html
-
+Content-Type: text/html\n
     " + data).bytes()) or {}
 }
 
